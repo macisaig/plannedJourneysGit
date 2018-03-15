80ee7331a5aae687bf4fadfee4a6ab28bc3a9cf8
macisaig <36687759+macisaig@users.noreply.github.com>
2018-03-14 20:22:44 -0400

    Added required to dates and changed color layout

---
+++ index.js
@@ -0,0 +1,38 @@
+$(function () {
+    var dateNow = new Date();
+
+    $('#toDate').datetimepicker({
+      minDate:dateNow,
+      stepping: 15
+    });
+    $('#fromDate').datetimepicker({
+      useCurrent: false, //Important! See issue #1075
+      minDate:dateNow,
+      stepping: 15
+    });
+    $("#toDate").on("dp.change", function (e) {
+        $('#fromDate').data("DateTimePicker").minDate(e.date);
+    });
+    $("#fromDate").on("dp.change", function (e) {
+        $('#toDate').data("DateTimePicker").maxDate(e.date);
+    });
+});
+
+function checkDates()
+ {
+	 var toDate = document.getElementById('toDateValue');
+	 var fromDate = document.getElementById('fromDateValue');
+	 var submitValue = false;
+	 
+	if ((toDate.value.length > 0) || (fromDate.value.length > 0))
+	{
+		submitValue = true;
+		document.getElementById("errDate").innerHTML = "";
+	}
+	else
+	{
+		document.getElementById("errDate").innerHTML = "<span class='smallWarning'><strong>*Please Enter a Start or End Date*<strong></span>";
+	}
+	
+	return submitValue;
+ }
