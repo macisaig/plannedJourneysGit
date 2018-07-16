fc2ced62191d4542a33d66f038ce754b6bc8b6e7
macisaig <macisaig.dukes@gmail.com>
2018-07-15 22:37:26 -0600

    Fixing up Plan a Trip

--- betaVersion/jsFiles/planATrip.js
+++ betaVersion/jsFiles/planATrip.js
@@ -3,9 +3,14 @@ $(function() {
     var start = moment();
     var end = moment().add(29, 'days');
     var minDate = moment();
+    var weekday = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+    var startDay;
+    var endDay;
 
     function cb(start, end) {
-        $('#datePicker span').html(start.format('MM/DD/YYYY') + ' - ' + end.format('MM/DD/YYYY'));
+        startDay = weekday[start.day()];
+        endDay = weekday[end.day()];
+        $('#datePicker span').html(startDay + " " + start.format('MM/DD') + ' - ' + endDay + " " + end.format('MM/DD'));
     }
 
     $('#datePicker').daterangepicker({
